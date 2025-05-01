6658a5d1052c49ac3b4929477a2d24379c929bca
Nishita-codes <nishitapatidar03@gmail.com>
2025-05-01 17:17:54 +0530

    added files

--- social-app/src/components/TopBar.jsx
+++ social-app/src/components/TopBar.jsx
@@ -1,6 +1,20 @@
+import "../App.css";
+import SearchIcon from '@mui/icons-material/Search';
+
 const TopBar = () => {
     return (
-      <div>TopBar</div>
+      <div className="topContainer">
+         <div className="topLeft">
+           <span className="logo">Friendzy</span>
+         </div>
+         <div className="topCenter">
+           <div className="searchbar">
+              <SearchIcon/>
+              <input type="text" className="searchInput" placeholder="Search for friends, post or video" />
+           </div>
+         </div>
+         <div className="topRight"></div>
+      </div>
     );
   };
   
